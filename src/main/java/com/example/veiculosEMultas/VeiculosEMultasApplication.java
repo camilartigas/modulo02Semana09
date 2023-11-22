95c2a1457915de925611a79eae420629f134ede2
Camila <camiart@gmail.com>
2023-11-22 19:25:36 -0300

    exercicio 01

---
+++ src/main/java/com/example/veiculosEMultas/VeiculosEMultasApplication.java
@@ -0,0 +1,13 @@
+package com.example.veiculosEMultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculosEMultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeiculosEMultasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/veiculosEMultas/VeiculosEMultasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.veiculosEMultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculosEMultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
